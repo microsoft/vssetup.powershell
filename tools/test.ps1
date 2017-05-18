9be0852ea65a9cc5adad7936fb30a01a8f066f4f
Heath Stewart <heaths@outlook.com>
2017-05-18 01:02:38 -0700

    Consolidate Docker configurations
    Use single Dockerfile for debugging, and store volume mappings in a single docker-compose.yml. AppVeyor will use a separate docker-compose.appveyor.yml to not set up Remote Debugger (not needed for PR and CI tests).

--- tools/test.ps1
+++ tools/test.ps1
@@ -9,7 +9,11 @@ param (
 
     [Parameter()]
     [ValidateNotNullOrEmpty()]
-    [string] $Platform = $env:PLATFORM
+    [string] $Platform = $env:PLATFORM,
+
+    [Parameter()]
+    [ValidateSet('Unit', 'Integration')]
+    [string[]] $Type = @('Unit', 'Integration')
 )
 
 if (-not $Configuration) {
@@ -20,80 +24,97 @@ if (-not $Platform) {
     $Platform = 'x86'
 }
 
-$ErrorActionPreference = 'SilentlyContinue'
+[bool] $Failed = $false
 
-# Find vstest.console.exe.
-$cmd = get-command vstest.console.exe | select-object -expand Path
-if (-not $cmd) {
-    $vswhere = get-childitem "$PSScriptRoot\..\packages\vswhere*" -filter vswhere.exe -recurse | select-object -first 1 -expand FullName
-    if (-not $vswhere) {
-        write-error 'Please run "nuget restore" on the solution to download vswhere.'
+if ($Type -contains 'Unit')
+{
+    # Find vstest.console.exe.
+    $cmd = get-command vstest.console.exe -ea SilentlyContinue | select-object -expand Path
+    if (-not $cmd) {
+        $vswhere = get-childitem "$PSScriptRoot\..\packages\vswhere*" -filter vswhere.exe -recurse | select-object -first 1 -expand FullName
+        if (-not $vswhere) {
+            write-error 'Please run "nuget restore" on the solution to download vswhere.'
+            exit 1
+        }
+
+        $path = & $vswhere -latest -requires Microsoft.VisualStudio.Component.ManagedDesktop.Core -property installationPath
+        if (-not $path) {
+            write-error 'No instance of Visual Studio found with vstest.console.exe. Please start a developer command prompt.'
+            exit 1
+        }
+
+        $cmd = join-path $path 'Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe'
+    }
+
+    if (-not (test-path $cmd)) {
+        write-error 'Could not find vstest.console.exe. Please start a developer command prompt.'
         exit 1
     }
 
-    $path = & $vswhere -latest -requires Microsoft.VisualStudio.Component.ManagedDesktop.Core -property installationPath
-    if (-not $path) {
-        write-error 'No instance of Visual Studio found with vstest.console.exe. Please start a developer command prompt.'
+    # Set up logger for AppVeyor.
+    $logger = if ($env:APPVEYOR -eq 'true') {
+        write-verbose 'Using AppVeyor logger when running in an AppVeyor build.'
+        '/logger:appveyor'
+    }
+
+    # Discover test assemblies for the current configuration.
+    $assemblies = get-childitem test -include *.test.dll -recurse | where-object {
+        $_.fullname -match "\\bin\\$Configuration\\"
+    } | foreach-object {
+        [string] $path = $_.FullName
+
+        write-verbose "Discovered test assembly '$path'."
+        "$path"
+    }
+
+    # Find the test adapter.
+    $adapter = get-childitem "$PSScriptRoot\..\packages\xunit.runner.visualstudio*" -filter xunit.runner.visualstudio.testadapter.dll -recurse | select-object -first 1 -expand FullName | foreach-object {
+        "/TestAdapterPath:`"$($_.FullName)`""
+    }
+
+    if (-not $adapter) {
+        write-warning 'Could not find test adapter. Unit tests may not be discovered.'
+    }
+
+    # Run unit tests.
+    & $cmd $logger $assemblies /parallel /platform:$Platform
+    if (-not $?) {
+        $Failed = $true
+    }
+}
+
+if ($Type -contains 'Integration')
+{
+    # Run docker integration tests.
+    if (get-command docker-compose -ea SilentlyContinue) {
+        [string] $path = if ($env:APPVEYOR -eq 'true') {
+            $no_tty = '-T'
+            resolve-path "$PSScriptRoot\..\docker\appveyor\docker-compose.yml"
+        } else {
+            resolve-path "$PSScriptRoot\..\docker\docker-compose.yml"
+        }
+
+        $verbose = if ($VerbosePreference -eq 'Continue') {
+            '--verbose'
+        }
+
+        write-verbose "Running tests in '$path'"
+        docker-compose -f "$path" $verbose run $no_tty --rm test -c Invoke-Pester C:\Tests -EnableExit -OutputFile C:\Tests\Results.xml -OutputFormat NUnitXml
+        if (-not $?) {
+            $Failed = $true
+        }
+
+        if ($env:APPVEYOR_JOB_ID) {
+            [string] $path = resolve-path "$PSScriptRoot\..\docker\Tests\Results.xml"
+            $url = "https://ci.appveyor.com/api/testresults/nunit/${env:APPVEYOR_JOB_ID}"
+
+            write-verbose "Uploading '$path' to '$url'"
+            $wc = new-object System.Net.WebClient
+            $wc.UploadFile($url, $path)
+        }
+    }
+
+    if ($Failed) {
         exit 1
     }
-
-    $cmd = join-path $path 'Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe'
-}
-
-if (-not (test-path $cmd)) {
-    write-error 'Could not find vstest.console.exe. Please start a developer command prompt.'
-    exit 1
-}
-
-# Set up logger for AppVeyor.
-$logger = if ($env:APPVEYOR -eq 'true') {
-    write-verbose 'Using AppVeyor logger when running in an AppVeyor build.'
-    '/logger:appveyor'
-}
-
-# Discover test assemblies for the current configuration.
-$assemblies = get-childitem test -include *.test.dll -recurse | where-object {
-    $_.fullname -match "\\bin\\$Configuration\\"
-} | foreach-object {
-    [string] $path = $_.FullName
-
-    write-verbose "Discovered test assembly '$path'."
-    "$path"
-}
-
-# Find the test adapter.
-$adapter = get-childitem "$PSScriptRoot\..\packages\xunit.runner.visualstudio*" -filter xunit.runner.visualstudio.testadapter.dll -recurse | select-object -first 1 -expand FullName | foreach-object {
-    "/TestAdapterPath:`"$($_.FullName)`""
-}
-
-if (-not $adapter) {
-    write-warning 'Could not find test adapter. Unit tests may not be discovered.'
-}
-
-# Run unit tests.
-& $cmd $logger $assemblies /parallel /platform:$Platform
-if (-not $?) {
-    exit $LASTEXITCODE
-}
-
-# Run docker integration tests in a separate process to set environment variables without affecting current process.
-if (get-command docker-compose -ea SilentlyContinue) {
-    [string] $path = resolve-path "$PSScriptRoot\..\docker\appveyor\docker-compose.yml"
-    write-verbose "Running tests in $path"
-
-    $verbose = if ($VerbosePreference -eq 'Continue') {
-        '--verbose'
-    }
-
-    docker-compose -f "$path" $verbose run -T test -c Invoke-Pester C:\Tests -EnableExit -OutputFile C:\Tests\Results.xml -OutputFormat NUnitXml
-    if ($env:APPVEYOR -eq 'true') {
-        [string] $path = resolve-path "$PSScriptRoot\..\docker\Tests\Results.xml"
-        $url = "https://ci.appveyor.com/api/testresults/nunit/${env:APPVEYOR_JOB_ID}"
-        write-verbose "Uploading '$path' to '$url'"
-
-        $wc = new-object System.Net.WebClient
-        $wc.UploadFile($url, $path)
-    }
-
-    exit $LASTEXITCODE
 }
